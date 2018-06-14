1b31c674fc157b0e3e92aaaa6c2cf6eafd4a611d
AndreasDM <AndreasDM@users.noreply.github.com>
2018-06-14 17:45:45 +0200

    changed regex

--- src/com/example/HtmlExtractor.java
+++ src/com/example/HtmlExtractor.java
@@ -22,9 +22,9 @@ class HtmlExtractor {
 
     HtmlExtractor(String htmlDocument) {
         this.namePattern = Pattern.
-                compile("\">\\b([^Om\\ ][A-Za-zæøå\\-]+\\ [A-Za-zæøå\\-]+(\\ [A-Za-zæøå\\-]+)?(\\ [A-Za-zæøå\\-]+)?)\\b<\\/a>");
+                compile(">\\b([A-Zæøåüö][a-zæøåüö]+?[\\ -][A-Zæøåüö][a-zæøåüö]+([\\ -][A-Zæøåüö][a-zæøåüö]{1,})?([\\ -][A-Zæøåüö][a-zæøåüö]+)?\\b)<");
         this.phonePattern = Pattern.
-                compile("\">([0-9]{3}\\ [0-9]{2}\\ [0-9]{3}|[0-9]{2}\\ [0-9]{2}\\ [0-9]{2}\\ [0-9]{2}\\ )<\\/span>");
+                compile(">\\b([0-9 ]{10,})<\\/span>");
         this.matcher = namePattern.matcher(htmlDocument);
         this.result = new ArrayList<>();
     }
@@ -55,7 +55,8 @@ class HtmlExtractor {
         while (matcher.find()) {
             if (count < result.size()) {
                 result.get(count).setPhone(matcher.group(1));
-                ++count;
+
+                count++;
             }
         }
     }
